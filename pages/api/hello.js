8b90e00da4dfee11281606dfcea834a4c6074aac
jakhub22 <jargalan.dev@gmail.com>
2023-06-07 16:56:29 +0800

    zas

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,5 +2,5 @@
 
 export default function handler(req, res) {
     console.log(process.env.WHITELIST, "process.env.WHITELIST");
-    res.status(200).json({ name: "John Doe" });
+    res.status(200).json({ name: "John Doe1" });
 }
