ff366f5dc2bddbafb18b0f56d65ef7b0f01ad7df
jakhub22 <jargalan.dev@gmail.com>
2023-06-07 12:01:07 +0800

    zasvar

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+    console.log(process.env.WHITELIST, "process.env.WHITELIST");
+    res.status(200).json({ name: "John Doe" });
 }
